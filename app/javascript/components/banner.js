8e3e9f91b3c67e1769e4d3d27c3e25ab059fc0fd
XiwayBanh <xiway.banh@gmail.com>
2021-04-02 20:03:09 +0800

    added style on homepage

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Let's binge watch some movies! ⬇ "],
+    strings: ["Let's binge watch some movies!"],
     typeSpeed: 100,
     loop: true
   });
